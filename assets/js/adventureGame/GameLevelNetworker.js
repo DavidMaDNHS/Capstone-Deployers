44019bccbbdf582896de10a077ab808460547932
THEGAMER221148 <weststefany@icloud.com>
2026-03-27 13:36:51 -0700

    added game level file

---
+++ assets/js/adventureGame/GameLevelNetworker.js
@@ -0,0 +1,100 @@
+// To build GameLevels, each contains GameObjects from below imports
+import GamEnvBackground from './GameEngine/GameEnvBackground.js';
+import Player from './GameEngine/Player.js';
+import Npc from './GameEngine/Npc.js';
+
+class GameLevelDesert {
+  constructor(gameEnv) {
+    let width = gameEnv.innerWidth;
+    let height = gameEnv.innerHeight;
+    let path = gameEnv.path;
+
+    // Background data
+    const image_src_desert = path + "/images/gamify/desert.png"; // be sure to include the path
+    const image_data_desert = {
+        name: 'desert',
+        greeting: "Welcome to the desert!  It is hot and dry here, but there are many adventures to be had!",
+        src: image_src_desert,
+        pixels: {height: 580, width: 1038}
+    };
+
+
+    // Player data for Chillguy
+    const sprite_src_chillguy = path + "/images/gamify/chillguy.png"; // be sure to include the path
+    const CHILLGUY_SCALE_FACTOR = 5;
+    const sprite_data_chillguy = {
+        id: 'Chill Guy',
+        greeting: "Hi I am Chill Guy, the desert wanderer. I am looking for wisdom and adventure!",
+        src: sprite_src_chillguy,
+        SCALE_FACTOR: CHILLGUY_SCALE_FACTOR,
+        STEP_FACTOR: 1000,
+        ANIMATION_RATE: 50,
+        INIT_POSITION: { x: 0, y: height - (height/CHILLGUY_SCALE_FACTOR) }, 
+        pixels: {height: 384, width: 512},
+        orientation: {rows: 3, columns: 4 },
+        down: {row: 0, start: 0, columns: 3 },
+        downRight: {row: 1, start: 0, columns: 3, rotate: Math.PI/16 },
+        downLeft: {row: 2, start: 0, columns: 3, rotate: -Math.PI/16 },
+        left: {row: 2, start: 0, columns: 3 },
+        right: {row: 1, start: 0, columns: 3 },
+        up: {row: 3, start: 0, columns: 3 },
+        upLeft: {row: 2, start: 0, columns: 3, rotate: Math.PI/16 },
+        upRight: {row: 1, start: 0, columns: 3, rotate: -Math.PI/16 },
+        hitbox: { widthPercentage: 0.45, heightPercentage: 0.2 },
+        keypress: { up: 87, left: 65, down: 83, right: 68 } // W, A, S, D
+    };
+
+    
+
+
+    // NPC data for Tux 
+    const sprite_src_tux = path + "/images/gamify/tux.png";
+    const sprite_greet_tux = "Hi I am Tux, the Linux mascot. I am very happy to spend some linux shell time with you!";
+    const sprite_data_tux = {
+        id: 'Tux',
+        greeting: sprite_greet_tux,
+        src: sprite_src_tux,
+        SCALE_FACTOR: 8,
+        ANIMATION_RATE: 50,
+        pixels: {height: 256, width: 352},
+        INIT_POSITION: { x: (width / 2), y: (height / 2)},
+        orientation: {rows: 8, columns: 11 },
+        down: {row: 5, start: 0, columns: 3 },
+        hitbox: { widthPercentage: 0.1, heightPercentage: 0.2 },
+        dialogues: [
+            "The terminal is a powerful tool. It's like having superpowers!",
+            "Did you know Linux powers most web servers and supercomputers?",
+            "ls, cd, mkdir, rm - these commands will become your best friends.",
+            "I love open source software! Freedom to study, modify, and share.",
+            "Have you tried using pipes to connect commands? It's magical!",
+            "Vim or Emacs? That's the eternal question among Linux users.",
+            "The penguin mascot represents the cold conditions of Finland where Linux was developed.",
+            "Linux was created by Linus Torvalds in 1991 while he was a student."
+        ],
+        reaction: function() {
+            // Use dialogue system instead of alert
+            if (this.dialogueSystem) {
+                this.showReactionDialogue();
+            } else {
+                console.log(sprite_greet_tux);
+            }
+        },
+        interact: function() {
+            // Show random dialogue message
+            if (this.dialogueSystem) {
+                this.showRandomDialogue();
+            }
+        }
+    };
+
+    // List of objects defnitions for this level
+    this.classes = [
+      { class: GamEnvBackground, data: image_data_desert },
+      { class: Player, data: sprite_data_chillguy },
+      { class: Npc, data: sprite_data_tux },
+    ];
+  }
+
+}
+
+export default GameLevelDesert;
